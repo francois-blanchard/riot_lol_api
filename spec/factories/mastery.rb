fb93864776529a838d3df05eea639b4a32600d9b
francois_blanchard <contact@fblanchard.fr>
2014-05-30 18:08:45 +0200

    version 0.0.4

--- spec/factories/mastery.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-	factory :mastery, :class => RiotLolApi::Model::Mastery do
-		id_mastery 37482369
-		name "support"
-		current false
-		talents nil 
-		initialize_with { new(:id_mastery => id_mastery, :name => name, :current => current, :talents => talents) }
-	end
-end
--- spec/factories/rune.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-	factory :rune, :class => RiotLolApi::Model::Rune do
-		id_rune 37482369
-		name "support"
-		current false
-		slots nil 
-		initialize_with { new(:id_rune => id_rune, :name => name, :current => current, :slots => slots) }
-	end
-end
