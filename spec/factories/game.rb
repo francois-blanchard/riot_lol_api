f2c3c29e50c8b5bdc70a2288069ea4715c74c177
francois_blanchard <contact@fblanchard.fr>
2014-10-22 23:48:05 +0200

    update data

--- spec/factories/game.rb
+++ spec/factories/game.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
 	factory :game, :class => RiotLolApi::Model::Game do
-		game_id 1462450355
+		game_id 1764353660
 		invalid false
 		game_mode "CLASSIC"
 		game_type "MATCHED_GAME"
 		sub_type "NORMAL"
 		map_id 1
 		team_id 200
-		champion_id 44
+		champion_id 40
 		spell1 4
-		spell2 7
+		spell2 3
 		level 30
-		ip_earned 52
-		create_date 1399415779754
+		ip_earned 235
+		create_date 1414008147108
 		fellow_players []
 		stats []
 		initialize_with { new(:game_id => game_id, :invalid => invalid, :game_mode => game_mode, :game_type => game_type, :sub_type => sub_type, :map_id => map_id, :team_id => team_id, :champion_id => champion_id, :spell1 => spell1, :spell2 => spell2, :level => level, :ip_earned => ip_earned, :create_date => create_date) }
--- spec/factories/summoner.rb
+++ spec/factories/summoner.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
 	factory :summoner, :class => RiotLolApi::Model::Summoner do
 		id 20639710
 		name "PacoLoco"
-		profile_icon_id 8
+		profile_icon_id 693
 		summoner_level 30
-		revision_date 1398345588000
+		revision_date 1414008147000
 		region "euw"
 		initialize_with { new(:id => id, :name => name, :profile_icon_id => profile_icon_id, :summoner_level => summoner_level, :revision_date => revision_date, :region => region) }
 	end
