5b22be3a211865170c3960ed071a217937e4a4d3
francois_blanchard <contact@fblanchard.fr>
2015-02-17 23:08:03 +0100

    add method match history

--- lib/riot_lol_api.rb
+++ lib/riot_lol_api.rb
@@ -4,6 +4,7 @@ require "riot_lol_api/clients"
 require "riot_lol_api/models"
 require "core_ext/string"
 require "core_ext/hash"
+require "riot_lol_api/model/class_base"
 
 module RiotLolApi
 	# # http://ddragon.leagueoflegends.com/tool/euw/fr_FR
--- lib/riot_lol_api/helper_classes.rb
+++ lib/riot_lol_api/helper_classes.rb
@@ -1,7 +1,22 @@
 module RiotLolApi
   module HelperClass
+
+    # Instance methods
+
     def list_methods
       self.class.instance_methods(false).map{|i| i.to_sym}
+    end
+
+    # Class methods
+
+    def self.included(object)
+      object.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def list_methods
+        self.instance_methods(false).map{|i| i.to_sym}
+      end
     end
   end
 end
