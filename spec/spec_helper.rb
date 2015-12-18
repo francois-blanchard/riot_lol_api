dc4a19db3c2a07f064930da8d1dc7553244696ed
FRB <francois.blanchard1@gmail.com>
2015-12-18 17:20:56 +0100

    test code climate average config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 require 'bundler/setup'
 require 'riot_lol_api' # and any other gems you need
 require 'webmock/rspec'
 require 'factory_girl'
-require "codeclimate-test-reporter"
 
-RiotLolApi::TOKEN = "KEY"
+RiotLolApi::TOKEN = 'KEY'
 
 Bundler.setup
 FactoryGirl.find_definitions
-
-CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
